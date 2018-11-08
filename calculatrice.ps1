27f25fbd906dbea9642e49de49349802a1f4e242
Talmec <baaldaske@gmail.com>
2018-11-08 11:15:55 +0100

    Calculatrice

---
+++ calculatrice.ps1
@@ -0,0 +1,32 @@
+﻿
+clear
+try{
+    [int]$chiffre1 = Read-Host "premier chiffre"}
+
+catch{
+    echo "ce n'est pas un chiffre"
+    sleep 2
+    exit 0 }
+
+    echo "addition mettez +"
+    echo "soustraction mettez -"
+    echo "multiplication mettez *"
+    echo "division mettez /"
+
+    $chiffre2 = Read-Host second chiffre
+
+    $operateur = Read-Host mettez votre operateur
+
+    Switch ($operateur){
+    "+"{$res = $chiffre1 + $chiffre2}
+    "-"{$res = $chiffre1 - $chiffre2}
+    "*"{$res = $chiffre1 * $chiffre2}
+    "/"{$res = $chiffre1 / $chiffre2}
+    default {
+    "error"
+    }
+    }
+
+
+ $res
+
