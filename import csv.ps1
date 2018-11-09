8d8c9452644d5d8934e9855398b802549be9c8aa
Talmec <baaldaske@gmail.com>
2018-11-09 15:35:23 +0100

    Jankenpon

---
+++ import csv.ps1
@@ -0,0 +1,18 @@
+﻿
+while($true)
+{
+$csv = Import-Csv "C:\département2.csv" -Delimiter ";"
+
+$userinput = Read-Host "quel est votre département"
+
+foreach($item in $csv)
+    {
+        $numdepart = $item.b
+        $nomdepart = $item.d
+
+    if ($numdepart -eq $userinput)
+    {
+        echo $nomdepart
+    }
+    }
+}
